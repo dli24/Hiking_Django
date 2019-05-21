60f90b4dd1debc2bff6d29cc910c89cb5ccaa60b
dli24 <dli24@buffalo.edu>
2019-05-21 11:26:52 -0700

    indent error fixed

--- hike/models.py
+++ hike/models.py
@@ -2,18 +2,8 @@ from django.db import models
 from accounts.models import Profile
 
 # Create your models here.
-
-class Comments(models.Model):
-  hike = models.ForeignKey(
-    Hike, on_delete=models.CASCADE, related_name='hike'
-  )
-  profile = models.ForeignKey(
-    Profile, on_delete=models.CASCADE, related_name='profile'
-  )
-  body = models.CharField(max_length=250)
-  rating = models.FloatField()
-
 class Hike(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hike')
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
     date = models.DateField()
@@ -26,14 +16,24 @@ class Hike(models.Model):
     distance = models.FloatField()
     difficulty = models.FloatField()
     picture = models.TextField()
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile')
 
     def __str__(self):
         return self.title
 
+        
+class Comments(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comment')
+    hike = models.ForeignKey(Hike, on_delete=models.CASCADE, related_name='comment')
+    body = models.CharField(max_length=250)
+    rating = models.FloatField()
+
+    def __str__(self):
+        return self.body
+
 class HikeGroup(models.Model):
-    hike = models.ForeignKey(Hike, on_delete=models.CASCADE, related_name='hike')
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile')
+    hike = models.ForeignKey(Hike, on_delete=models.CASCADE, related_name='hike_group')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hike_group')
 
     def __str__(self):
         return self.profile
+
