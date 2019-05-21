ac20788f9d1929c4871508c00198a17a00fcd121
Ridwan sarwari <r.sarwari@vigilvideo.com>
2019-05-21 10:47:13 -0700

    created prifile model and comments model

--- hike/models.py
+++ hike/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import Profile
 
 # Create your models here.
+
+class Comments(models.Model):
+  hike = models.ForeignKey(
+    Hike, on_delete=models.CASCADE, related_name='hike'
+  )
+  profile = models.ForeignKey(
+    Profile, on_delete=models.CASCADE, related_name='profile'
+  )
+  body = models.CharField(max_length=250)
+  rating = models.FloatField()
+  
