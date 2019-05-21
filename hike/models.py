89f59a13c0c6446f6f0a1a47f3e0bf0dbb7bda12
dli24 <dli24@buffalo.edu>
2019-05-21 10:47:04 -0700

    models boiler plate

--- hike/models.py
+++ hike/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from accounts.models import Profile
+
 
 # Create your models here.
+
+class Hike(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    date = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zipcode = models.IntegerField()
+    distance = models.FloatField()
+    difficulty = models.FloatField()
+    picture = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return self.title
+
+class HikeGroup(models.Model):
+    hike = models.ForeignKey(Hike, on_delete=models.CASCADE, related_name='hike')
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return self.profile
