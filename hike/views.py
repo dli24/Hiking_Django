c71c48852b4a458b5270bff08c37d5b98c9a0054
Ridwan sarwari <r.sarwari@vigilvideo.com>
2019-05-22 15:59:03 -0700

    add a hike

--- hike/views.py
+++ hike/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Hike, Comments, HikeGroup
 from django.contrib.auth.decorators import login_required
+from .forms import HikeForm
 
 # Create your views here.
 # HIKE VIEWS
@@ -17,8 +18,19 @@ def hike_detail(request):
 
 #add a new hike
 # create a form to add a new hike
+
+@login_required
 def hike_new(request):
-  return render(request, 'hike/hike_new.html')
+  if request.method == 'POST':
+    form = HikeForm(request.POST)
+    if form.is_valid():
+      hike = form.save(commit=False)
+      hike.user = request.user
+      hike.save()
+      return redirect('hike_detail', pk=hike.pk)
+  else:
+      form = HikeForm()
+  return render(request, 'hike/hike_form.html', {'form': form})
 
 # show all hikes on a calendar
 def hike_calendar(request):
