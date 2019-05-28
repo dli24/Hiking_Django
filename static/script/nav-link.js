1fd8141d530630ddf62c3f50a3967b2ebecb423f
mpetty <michael.petty@gmail.com>
2019-05-27 20:57:56 -0700

    move nav-link.js to base; move nav style into css activeNav

--- static/script/nav-link.js
+++ static/script/nav-link.js
@@ -1,5 +1,5 @@
 $('.nav-link').each(function(){
     if($(this).prop('href') == window.location.href){
-        $(this).css({"color":"white","background-color":"#4169e1","text-decoration":"underline"})
+        $(this).addClass('activeNav');
     }
 })
